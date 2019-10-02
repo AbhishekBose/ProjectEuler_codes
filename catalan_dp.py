afea9602d7dddeee13cfcddc44dcc5cbeddc27cf
HimanshuGarg <hghimanshu81@gmail.com>
2019-10-02 17:36:13 +0530

    catalan dp

---
+++ catalan_dp.py
@@ -0,0 +1,13 @@
+def catalan(n):
+    if n==0:
+        return 1
+    elif n==1:
+        return 1
+    else:
+        catalan_array = [0]*(n+1)
+        catalan_array[0] = 1
+        catalan_array[1] = 1
+        for i in range(2,n+1):
+            catalan[i] = 0
+            for j in range(i)catalan[i] = catalan[i]+(catalan[])
+
